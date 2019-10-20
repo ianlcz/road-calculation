f239828dd7aee48caf57bda621edac22c2949a2a
ianlcz <yann.lecoz@ynov.com>
2019-10-21 01:33:51 +0200

    UPLOAD 21/10/2019
    Amélioration du code en rajoutant certaines vérifications pour empêcher des bugs

---
+++ funcs/tools.php
@@ -0,0 +1,63 @@
+<?php
+    /**
+     * Retrieve on a site the distance in meter between two cities
+     *
+     * @param string $departureCity
+     * @param string $arrivalCity
+     * @return float
+     */
+    function retrieve_distance(string $departureCity, string $arrivalCity):float
+    {
+        // Processing to extracte data from the code of the page
+        $data = explode('<div class="value">', file_get_contents('https://www.bonnesroutes.com/distance/?from=' . $departureCity . '&to=' . $arrivalCity));
+        // Selection of distance data
+        $data = explode('</div>', $data[3]);
+        // Return of the distance converted into float
+        return $distance = floatval($data[0]) * 1000;
+    }
+
+    /**
+     * Convert speed in kilometer per hour to meter per second
+     *
+     * @param float $number
+     * @return float
+     */
+    function convert_kmh_to_ms(float $number):float
+    {
+        return $number * 1000 / 3600;
+    }
+
+    /**
+     * Calculate the time to do a given distance
+     *
+     * @param string $departureCity
+     * @param string $arrivalCity
+     * @return float
+     */
+    function calculate_time(string $departureCity, string $arrivalCity):float
+    {
+        // Calculate the time needed to accelerate or deccelerate
+        $timeToAccelerateOrDecelerate = convert_kmh_to_ms(90) / convert_kmh_to_ms(10) * 60;
+        // Calculate the distance traveled during acceleration and deceleration
+        $traveledDistance = 1/2 * (convert_kmh_to_ms(90)/$timeToAccelerateOrDecelerate) * pow($timeToAccelerateOrDecelerate, 2) * 2;
+        // Calculate the average speed
+        $averageSpeed = $traveledDistance / ($timeToAccelerateOrDecelerate * 2);
+        // Calculate the travel time without a break if the distance is less than the distance traveled
+        if(retrieve_distance($departureCity, $arrivalCity) < $traveledDistance)
+        {
+            return $timeWithoutBreak = retrieve_distance($departureCity, $arrivalCity) / $averageSpeed;
+        }
+        // Otherwise calculate the travel time with breaks
+        else
+        {
+            // Calculate the remaining distance to travel
+            $remainingDistance = retrieve_distance($departureCity, $arrivalCity) - $traveledDistance;
+            // Full time to travel this distance without breaks
+            $timeWithoutBreak = 2 * $timeToAccelerateOrDecelerate + $remainingDistance / convert_kmh_to_ms(90);
+            // Calculates the number of breaks to be made during the path
+            $numberOfBreak = intval($timeWithoutBreak / (2 * 3600));
+            // Full time to travel this distance with breaks
+            return $timeWithBreak = ($timeWithoutBreak + $numberOfBreak * 15 * 60);
+        }
+    }
+?>
--- index.php
+++ index.php
@@ -1,69 +1,6 @@
 <?php
-    $villeDepart = 'Bordeaux';
-    $villeArrivee = 'Douarnenez';
-
-    /**
-     * Retrieve on a site the distance in meter between two cities
-     *
-     * @param string $departureCity
-     * @param string $arrivalCity
-     * @return float
-     */
-    function retrieve_distance(string $departureCity, string $arrivalCity):float
-    {
-        // Traitement des données récupérées du code de la page
-        $datas = explode('<div class="value">', file_get_contents('https://www.bonnesroutes.com/distance/?from=' . $departureCity . '&to=' . $arrivalCity));
-        // On choisit les données de distance
-        $datas = $datas[3];
-        // On récupère la valeur de la distance
-        $datas = explode('</div>', $datas);
-        // On retourne la valeur de la distance convertie en float
-        return $distance = floatval($datas[0]) * 1000;
-    }
-
-    /**
-     * Convert speed in kilometer per hour to meter per second
-     *
-     * @param float $number
-     * @return float
-     */
-    function convert_kmh_to_ms(float $number):float
-    {
-        return $number * 1000 / 3600;
-    }
-
-    function calculate_time(string $departureCity, string $arrivalCity):float
-    {
-        // On calcule le temps nécessaire pour accéler ou décélérer
-        $tempsPourAccelererOuDecelerer = convert_kmh_to_ms(90) / convert_kmh_to_ms(10) * 60;
-        // On calcule la distance parcourue pendant l'accélération et la décélération
-        $distanceParcourue = 1/2 * (convert_kmh_to_ms(90)/$tempsPourAccelererOuDecelerer) * pow($tempsPourAccelererOuDecelerer, 2) * 2;
-        $vitesseMoyenne = $distanceParcourue / ($tempsPourAccelererOuDecelerer*2);
-
-
-        // Si la distance est inférieur à $distanceParcourue
-        if(retrieve_distance($departureCity, $arrivalCity) < $distanceParcourue)
-        {
-            return $tempsSansPause = retrieve_distance($departureCity, $arrivalCity) / $vitesseMoyenne;
-        }
-        // Sinon on calcule le temps de trajet avec des pauses
-        else
-        {
-            // On calcule la distance restante à parcourir
-            $distanceRestante = retrieve_distance($departureCity, $arrivalCity) - $distanceParcourue;
-            // On calcule en combien de temps il doit accomplir cette distance
-            $tempsPourFaireDistanceRestante = $distanceRestante / convert_kmh_to_ms(90);
-            // Temps complet pour parcourir cette distance sans pause
-            $tempsSansPause = 2 * $tempsPourAccelererOuDecelerer + $tempsPourFaireDistanceRestante;
-
-            // On calcule le nombre de pause à faire durant le trajet
-            $nombreDePause = intval($tempsSansPause / (2 * 3600));
-            // Temps complet pour parcourir cette distance avec pause
-            return $tempsAvecPause = ($tempsSansPause + $nombreDePause * 15 * 60);
-        }
-    }
+    require_once('./funcs/tools.php');
 ?>
-
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -74,9 +11,43 @@
     <title>Road Calculation | Yann Le Coz</title>
 </head>
 <body>
-    <p>Ville de départ: <?php echo $villeDepart; ?></p>
-    <p>Ville d'arrivée: <?php echo $villeArrivee; ?></p>
-    <p>Distance entre les deux villes: <?php echo retrieve_distance($villeDepart, $villeArrivee); ?></p>
-    <p>Temps complet: <?php echo strftime("%H/%M", calculate_time($villeDepart, $villeArrivee)); ?></p>
+    <form methode='POST'>
+        <label for="departureCity">Entrez votre ville de départ: </label>
+        <input type="text" name="departureCity">
+
+        <label for="arrivalCity">Entrez votre ville d'arrivée: </label>
+        <input type="text" name="arrivalCity">
+
+        <input type="submit" name="sendinfos" value="Calculer la temps du trajet">
+    </form>
+    <?php if(!empty($_GET['departureCity']) AND !empty($_GET['arrivalCity'])){ ?>
+        <?php if($_GET['departureCity'] !== $_GET['arrivalCity']){ ?>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Ville de départ</th>
+                        <th>Ville d'arrivée</th>
+                        <th>Distance parcourue (en km)</th>
+                        <th>Temps (en HH/mm)</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td><?php echo $_GET['departureCity']; ?></td>
+                        <td><?php echo $_GET['arrivalCity']; ?></td>
+                        <td><?php echo retrieve_distance($_GET['departureCity'], $_GET['arrivalCity']) / 1000; ?></td>
+                        <td><?php echo strftime("%H/%M", calculate_time($_GET['departureCity'], $_GET['arrivalCity'])); ?></td>
+                    </tr>
+                </tbody>
+            </table>
+        <?php } else { ?>
+            <p>Eurreur: Vos villes de départ et d'arrivée sont identiques</p>
+        <?php } ?>
+    <?php } else { ?>
+        <p>Erreur: des champs n'ont pas été complétés</p>
+    <?php } ?>
+    <footer>
+        &copy; <?php echo strftime("%Y"); ?><span style="margin-left: 0.6rem;">Yann Le Coz</span>
+    </footer>
 </body>
 </html>
