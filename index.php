95dfd6e66ba6a82a824397d48e6fa2c058976453
ianlcz <yann.lecoz@ynov.com>
2019-10-21 18:21:58 +0200

    UPLOAD 21/10/2019
    CSS bien entamé.
    Reste à modifier le favicon et mettre le table au centre de la page.

--- index.php
+++ index.php
@@ -7,18 +7,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css" />
     <link rel="icon" type="image/png" href="favicon.png" />
+
     <title>Road Calculation | Yann Le Coz</title>
 </head>
 <body>
     <form methode='POST'>
-        <label for="departureCity">Entrez votre ville de départ: </label>
-        <input type="text" name="departureCity">
-
-        <label for="arrivalCity">Entrez votre ville d'arrivée: </label>
-        <input type="text" name="arrivalCity">
-
-        <input type="submit" name="sendinfos" value="Calculer la temps du trajet">
+        <input type="text" name="departureCity" placeholder="Entrez votre ville de départ">
+        <input type="text" name="arrivalCity" placeholder="Entrez votre ville d'arrivée">
+        <br/>
+        <button type="submit" name="sendinfos" value="Calculer la temps du trajet">Calculer la temps du trajet</button>
     </form>
     <?php if(!empty($_GET['departureCity']) AND !empty($_GET['arrivalCity'])){ ?>
         <?php if($_GET['departureCity'] !== $_GET['arrivalCity']){ ?>
@@ -27,7 +26,7 @@
                     <tr>
                         <th>Ville de départ</th>
                         <th>Ville d'arrivée</th>
-                        <th>Distance parcourue (en km)</th>
+                        <th>Distance parcourue</th>
                         <th>Temps (en HH/mm)</th>
                     </tr>
                 </thead>
@@ -35,7 +34,7 @@
                     <tr>
                         <td><?php echo $_GET['departureCity']; ?></td>
                         <td><?php echo $_GET['arrivalCity']; ?></td>
-                        <td><?php echo retrieve_distance($_GET['departureCity'], $_GET['arrivalCity']) / 1000; ?></td>
+                        <td><?php echo retrieve_distance($_GET['departureCity'], $_GET['arrivalCity']) / 1000 . " km"; ?></td>
                         <td><?php echo strftime("%H/%M", calculate_time($_GET['departureCity'], $_GET['arrivalCity'])); ?></td>
                     </tr>
                 </tbody>
@@ -43,8 +42,6 @@
         <?php } else { ?>
             <p>Eurreur: Vos villes de départ et d'arrivée sont identiques</p>
         <?php } ?>
-    <?php } else { ?>
-        <p>Erreur: des champs n'ont pas été complétés</p>
     <?php } ?>
     <footer>
         &copy; <?php echo strftime("%Y"); ?><span style="margin-left: 0.6rem;">Yann Le Coz</span>
